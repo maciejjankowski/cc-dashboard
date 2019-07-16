e8781168e0b32edf3fc9149941f33a827af37569
mj <maj1337+git@gmail.com>
2019-07-16 13:33:05 +0200

    init comit, skeleton codd

---
+++ server.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, request, render_template, jsonify
+
+# Support for gomix's 'front-end' and 'back-end' UI.
+app = Flask(__name__, static_folder='public', template_folder='views')
+
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
